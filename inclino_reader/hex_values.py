fb2f00d8bc87defea066a3275e1ebf063ba8434e
Michal Nawrocki <michal.m.nawrocki@gmail.com>
2020-09-27 16:16:44 +0200

    [2/Feature] Add HexCode class to store sensor's input codes

--- inclino_reader/hex_values.py
+++ inclino_reader/hex_values.py
@@ -2,4 +2,17 @@
 Keep all the HEX codes in here so they can be reused
 """
 
-# TODO: Add HEX codes from the inclinometer documentation
+
+class HexCode:
+    def __init__(self, hex_code: bytearray, read_length: int):
+        """
+        Keep track of the hex code the sensor expects and what's the
+        length of it's output
+
+        :param hex_code: The HEX value for an operation
+        :param read_length: The length the sensor's response for given command
+        """
+        self.hex_code = hex_code
+        self.read_length = read_length
+
+
