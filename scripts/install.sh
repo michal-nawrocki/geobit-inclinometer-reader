f17cb06f9a224d374f646105704959e6893c3686
michal.nawrocki <michal.m.nawrocki@gmail.com>
2022-10-26 02:29:21 +0200

    [2022.10.26] Modify install.sh to add bash aliases

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,13 @@
 # sudo cp -rf . /var/lib/inclinometer-reader
 
 # Setup store location of the readings
-mkdir ~/.geobit/
-touch ~/.geobit/readings.txt
+# mkdir ~/.geobit/
+# touch ~/.geobit/readings.txt
 
 # Enable Cronjob to read every 5 minutes
+# */5 * * * * bash /home/admin/workspace/geobit-inclinometer-reader/scripts/run.sh >/dev/null 2>&1
 
-
+echo "### Installed by geobit-inclinometers ###" | tee -a ~/.bashrc > /dev/null
+echo "alias readings=\"tail ~/.geobit/readings.txt\"" | tee -a ~/.bashrc > /dev/null
+echo "alias make_measurement=\"bash ~/workspace/geobit-inclinometer-reader/scripts/read.sh\"" | tee -a ~/.bashrc > /dev/null
+source ~/.bashrc
